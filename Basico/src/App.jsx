7fb0751e0c3765b12d6a0fcdcdee3bd7d59b66da
Diogo Dotto <diogodotto@hotmail.com>
2024-08-29 22:46:38 -0300

    utilizando Arrays

--- Basico/src/App.jsx
+++ Basico/src/App.jsx
@@ -2,33 +2,36 @@ import { Component } from "react"
 
 export default class App extends Component {
     state = {
-        name: 'Diogo Dotto',
-        counter: 0
+        posts: [
+            {
+                id: 1,
+                title: 'O Título 1',
+                body: 'O corpo 1'
+            },
+            {
+                id: 2,
+                title: 'O Título 2',
+                body: 'O corpo 2'
+            },
+            {
+                id: 3,
+                title: 'O Título 3',
+                body: 'O corpo 3'
+            },
+        ]
     };
 
-    handlePClick = () => {
-        this.setState({ name: 'Roger' })
-    }
-
-    handleAClick = (event) => {
-        event.preventDefault();
-        const { counter } = this.state;
-        this.setState({ counter: counter + 1 })
-    }
-
-
     render() {
-        const { name, counter } = this.state;
+        const { posts } = this.state;
 
         return (
-            <div>
-                <p onClick={this.handlePClick}>
-                    {name} { counter }
-                </p>
-
-                <a onClick={this.handleAClick}>
-                    Clique do contador
-                </a>
+            <div className="App">
+                {posts.map(post => (
+                    <div key={post.id}>
+                        <h1 >{post.title}</h1>
+                        <p>{post.body}</p>
+                    </div>
+                ))}
             </div>
         )
     }
